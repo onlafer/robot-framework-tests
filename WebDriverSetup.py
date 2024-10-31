daac101251c10ec3637850133f269551dcb0360c
onlafer <159691117+onlafer@users.noreply.github.com>
2024-11-01 01:23:26 +0300

    Update WebDriverSetup.py

--- WebDriverSetup.py
+++ WebDriverSetup.py
@@ -6,6 +6,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_chrome_driver_path():
     return os.path.normpath(ChromeDriverManager().install())
 
-
 def create_service(chrome_driver_path: str) -> Service:
     return Service(executable_path=chrome_driver_path)
